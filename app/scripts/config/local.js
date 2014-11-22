1c8e043c35c315ea2c71fb6c24db3be4076a61a7
Leonardo Loch Zanivan <pangalz@gmail.com>
2014-11-22 14:55:03 -0200

    Demonstração

--- app/scripts/config/local.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular._localConfig = {
-    environment: 'development',
-    API: {
-        useMocks: true,
-        fakeDelay: 100
-    }
-};
--- app/scripts/config/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('app')
-    .config(function ($urlRouterProvider) {
-        $urlRouterProvider.when('', '/');
-        $urlRouterProvider.otherwise('/error?code=404');
-    });
